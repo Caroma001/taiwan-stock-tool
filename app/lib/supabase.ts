0b950f3178644ecea1d457a4f3c0dab3f239079b
Bruce3689 <caroma001@gmail.com>
2026-06-24 22:34:49 +0700

    Connect Supabase stocks

---
+++ app/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://fxpblbcatetemprxusao.supabase.co",
+  "sb_publishable_hr75rxe0c7cBtuR0ajyyZw_0DZzlUaL"
+);
